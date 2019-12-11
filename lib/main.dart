186948af163b40f56a15994fc400f47bbfe2ea55
wucha <wuchao@chuangjiangx.com>
2019-12-11 19:04:17 +0800

    feat:订单列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gas_station/pages/order/order_List.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return new OrderList();
+    }));
+    
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -105,6 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
+        
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
---
+++ lib/pages/order/order_List.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gas_station/pages/order/order_list_bean.dart';
+
+class OrderList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new OrderListState();
+  }
+}
+
+class OrderListState extends State{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      body: _buildOrder(),);
+  }
+
+  Widget _buildOrder(){
+    final _items = _createData();
+    return new ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (context,position){
+          if(position.isOdd){
+            return new Divider();
+          }
+          return _buildOrderItem(_items[position]);
+        });
+  }
+
+  Widget _buildOrderItem(OrderListBean item) {
+    return new ListTile(
+      leading: new Container(
+        padding: const EdgeInsets.all(15.0) ,
+        child: new Text('${item.gasType}',
+        style: new TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),),
+      decoration: new BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle,
+      ),),
+      title: new Text(
+        item.payType,
+        style: new TextStyle(
+            color: Colors.black87,
+            fontSize: 18.0),),
+      subtitle: new Container(
+        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+        child: new Text(
+          item==null?'':item.createTime,
+          style: new TextStyle(
+              color: Colors.grey[400],
+              fontSize: 16.0),),
+      ),
+      trailing: new Container(
+        child: new Column(children: <Widget>[
+        new Text(
+            '￥${item.amount}',
+              style: new TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w700
+              ),),
+        new Container(
+          padding: const EdgeInsets.all(2.0),
+          child: new Text(
+            item.status,
+            style:new TextStyle(
+              color: Colors.orange,
+              fontSize: 16.0,
+
+            )),),
+      ],),),
+      onTap: _jump(),
+      
+    );
+  }
+
+  List<OrderListBean> _createData() {
+    final _items=new List<OrderListBean>();
+    for(int i=0;i<100;i++){
+      final index = i%3;
+      final orderListBean=new OrderListBean();
+      if(index==0){
+        orderListBean.gasType='92#';
+        orderListBean.amount=200;
+        orderListBean.createTime='2019-12-11 18:04:00';
+        orderListBean.payType='柴油卡支付';
+        orderListBean.status='支付成功';
+      }else if(index==1){
+        orderListBean.gasType='92#';
+        orderListBean.amount=100;
+        orderListBean.createTime='2019-12-11 18:04:00';
+        orderListBean.payType='刷卡支付';
+        orderListBean.status='退款成功';
+      }else{
+        orderListBean.gasType='96#';
+        orderListBean.amount=300;
+        orderListBean.createTime='2019-12-11 18:04:00';
+        orderListBean.payType='汽油卡支付';
+        orderListBean.status='部分退款';
+      }
+      _items.add(orderListBean);
+    }
+
+    return _items;
+  }
+
+  //跳转到详情
+  _jump() {
+
+  }
+
+}
+
