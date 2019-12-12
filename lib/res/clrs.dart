5115a53cdd9b4894482c177aafd86daef44a592e
杨水强 <yangshuiqiang@chuangjiangx.com>
2019-12-12 11:57:17 +0800

    merge:统一文字主题

--- lib/res/clrs.dart
+++ lib/res/clrs.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Clrs {
+  //全局颜色
   static const Color colorPrimary = Color(0xff1890FF);
   static const Color colorAccent = Color(0xffcccccc);
   static const Color backgroundColor = Color(0xffF4F4F4);
+  static const Color appBarLightBg = Color(0xFFFAFAFA);
+  static const Color alphaColorPrimary = Color(0x1C1890FF);
 
+  //分割线
   static const Color divide = Color(0xffDDDDDD);
+  static const Color divide2 = Color(0xFF979797);
 
+  //文字颜色
   static const Color textBlue = Color(0xff1890FF);
   static const Color textBlueDark = Color(0xff005DFD);
   static const Color textOrange = Color(0xffFB7B1D);
   static const Color textOrangeDark = Color(0xffF86A02);
   static const Color textBlack = Color(0xff333333);
-
   static const Color textBlackSub = Color(0xff888888);
   static const Color textBlackLight = Color(0xffcccccc);
-  
-  static const Color appBarLightBg = Color(0xFFFAFAFA);
-  static const Color divide2 = Color(0xFF979797);
-  static const Color alphaColorPrimary = Color(0x1C1890FF);
-
 }
