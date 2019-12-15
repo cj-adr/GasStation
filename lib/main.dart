ff336e261e385f758057943938bca1d15b67154d
wucha <wuchao@chuangjiangx.com>
2019-12-15 10:40:10 +0800

    feat:订单列表，订单详情

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:gas_station/pages/member/detail/member_detail_page.dart';
 import 'package:gas_station/res/themes.dart';
 
 void main() {
