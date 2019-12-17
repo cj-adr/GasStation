3773ba7e4b21e5906e2660f6c1082349ce5adbcb
shenyou <shenyou>
2019-12-17 20:10:57 +0800

    feat:CommonButton

---
+++ lib/components/common_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CommonButton extends StatefulWidget {
+  CommonButton(
+      {Key key, this.content = '确定', @required this.onPressed, this.margin})
+      : super(key: key);
+
+  String content;
+
+  VoidCallback onPressed;
+
+  EdgeInsetsGeometry margin;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CommonButtonState();
+  }
+}
+
+class _CommonButtonState extends State<CommonButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 40,
+        margin: widget.margin == null
+            ? EdgeInsets.fromLTRB(40, 0, 40, 0)
+            : widget.margin,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(3), color: Colors.lightBlue),
+        child: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: FlatButton(
+            onPressed: _onPressed,
+            child: Text(
+              widget.content,
+              style: TextStyle(fontSize: 14, color: Colors.white),
+            ),
+          ),
+        ));
+  }
+
+  void _onPressed() {
+    widget.onPressed();
+  }
+}
