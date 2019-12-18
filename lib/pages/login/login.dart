5026041e45f0d74568b58ff6c7413105f9742135
杨水强 <yangshuiqiang@chuangjiangx.com>
2019-12-18 19:39:23 +0800

    refactor:代码优化

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -3,14 +3,13 @@ import 'package:gas_station/pages/login/code_login.dart';
 import 'package:gas_station/pages/login/psd_login.dart';
 
 class LoginPage extends StatelessWidget {
-
   //登录类型：0，验证码登录；1，密码登录
   int type;
 
-  LoginPage({this.type});
+  LoginPage({this.type = 1});
 
   @override
   Widget build(BuildContext context) {
-    return (type == 0 ||type == null) ? CodeLoginPage() : PsdLoginPage();
+    return (type == 0 || type == null) ? CodeLoginPage() : PsdLoginPage();
   }
 }
