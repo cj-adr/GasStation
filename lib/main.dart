a212c6e008104f744e51f868f5309a3e1149e7af
杨水强 <yangshuiqiang@chuangjiangx.com>
2019-12-18 16:37:28 +0800

    refactor:代码优化

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gas_station/pages/home/home_page.dart';
-import 'package:gas_station/pages/login/login.dart';
-import 'package:gas_station/pages/test/test_page.dart';
 import 'package:gas_station/res/themes.dart';
 import 'package:gas_station/router/route_manager.dart';
 
