113e45907e38156bb41894bcc33e03ac7b55bb15
杨水强 <yangshuiqiang@chuangjiangx.com>
2019-12-27 17:53:01 +0800

    feat:框架调整

--- lib/architecture/base_model.dart
+++ lib/architecture/base_model.dart
@@ -1,7 +1,18 @@
+import 'package:dio/dio.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class BaseModel extends Model {
+  CancelToken _cancelToken;
+
+  get cancelToken => _cancelToken;
+
+  BaseModel() {
+    _cancelToken = CancelToken();
+  }
+
   void onStart() {}
 
-  void onStop() {}
+  void onStop() {
+    _cancelToken.cancel();
+  }
 }
